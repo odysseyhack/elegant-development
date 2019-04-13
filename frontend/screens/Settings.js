a01aa8e3c5b98fd3ff6190fb8507b640bb2741aa
Dahir Warsame <dahirwarsame@gmail.com>
2019-04-13 11:46:14 +0200

    added register and new account pages

--- frontend/screens/Settings.js
+++ frontend/screens/Settings.js
@@ -1,196 +1,197 @@
-import React, { Component } from "react";
-import { Image, StyleSheet, ScrollView, TextInput } from "react-native";
-
-import { Divider, Button, Block, Text, Switch } from "../components";
-import { theme, mocks } from "../constants";
+import React, {Component} from 'react'
+import {Image, StyleSheet, ScrollView, TextInput} from 'react-native'
+import {Divider, Button, Block, Text, Switch} from '../components';
+import {theme, mocks} from '../constants';
+import { CircularProgress } from 'react-native-circular-progress';
 
 class Settings extends Component {
-  state = {
-    budget: 850,
-    monthly: 1700,
-    notifications: true,
-    newsletter: false,
-    editing: null,
-    profile: {}
-  };
-
-  componentDidMount() {
-    this.setState({ profile: this.props.profile });
-  }
-
-  handleEdit(name, text) {
-    const { profile } = this.state;
-    profile[name] = text;
-
-    this.setState({ profile });
-  }
-
-  toggleEdit(name) {
-    const { editing } = this.state;
-    this.setState({ editing: !editing ? name : null });
-  }
-
-  renderEdit(name) {
-    const { profile, editing } = this.state;
-
-    if (editing === name) {
-      return (
-        <TextInput
-          defaultValue={profile[name]}
-          onChangeText={text => this.handleEdit([name], text)}
-        />
-      );
+    state = {
+        budget: 850,
+        monthly: 1700,
+        withdrawMoney: false,
+        changeTargetDate: true,
+        changebudgetpercentage: true,
+        editing: null,
+        account: {},
     }
 
-    return <Text bold>{profile[name]}</Text>;
-  }
 
-  render() {
-    const { profile, editing } = this.state;
+    componentDidMount() {
+        this.setState({account: this.props.account});
+    }
 
-    return (
-      <Block>
-        <Block flex={false} row center space="between" style={styles.header}>
-          <Text h1 bold>
-            Settings
-          </Text>
-          <Button>
-            <Image source={profile.avatar} style={styles.avatar} />
-          </Button>
-        </Block>
+    handleEdit(name, text) {
+        const {account} = this.state;
+        account[name] = text;
 
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Block style={styles.inputs}>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  Username
-                </Text>
-                {this.renderEdit("username")}
-              </Block>
-              <Text
-                medium
-                secondary
-                onPress={() => this.toggleEdit("username")}
-              >
-                {editing === "username" ? "Save" : "Edit"}
-              </Text>
+        this.setState({account: account});
+    }
+
+    toggleEdit(name) {
+        const {editing} = this.state;
+        this.setState({editing: !editing ? name : null});
+    }
+    Capitalize(str){
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    renderEdit(name) {
+        const {account, editing} = this.state;
+
+        if (editing === name) {
+            return (
+                <TextInput
+                    defaultValue={name}
+                    onChangeText={text => this.handleEdit([name], text)}
+
+                />
+            )
+        }
+
+        return <Text bold>{"Please enter the "+this.Capitalize(name)    }</Text>
+    }
+
+    render() {
+        const {account, editing} = this.state;
+
+        return (
+            <Block>
+                <Block flex={false} row center space="between" style={styles.header}>
+                    <Text h1 bold>New Account</Text>
+                </Block>
+                <Block center>
+                    <CircularProgress
+                        size={214} // can use  with * .5 => 50%
+                        fill={85} // percentage
+                        lineCap="round" // line ending style
+                        rotation={220}
+                        arcSweepAngle={280}
+                        width={theme.sizes.base}
+                        tintColor={theme.colors.primary} // gradient is not supported :(
+                        backgroundColor={theme.colors.gray3}
+                        backgroundWidth={theme.sizes.base / 2}
+                    >
+                        {() => (
+                            <Block center middle>
+                                <Text h2 medium>7.2</Text>
+                                <Text h3 transform="uppercase">Fair</Text>
+                            </Block>
+                        )}
+                    </CircularProgress>
+                </Block>
+                <Block center>
+                </Block>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <Divider margin={[theme.sizes.base, theme.sizes.base * 2]}/>
+                    <Block style={styles.inputs}>
+                        <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                            <Block>
+                                <Text gray2 style={{marginBottom: 10}}>Label</Text>
+                                {this.renderEdit('label')}
+                            </Block>
+                            <Text medium secondary onPress={() => this.toggleEdit('label')}>
+                                {editing === 'label' ? 'Save' : 'Edit'}
+                            </Text>
+                        </Block>
+                        <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                            <Block>
+                                <Text gray2 style={{marginBottom: 10}}>Target</Text>
+                                {this.renderEdit('target')}
+                            </Block>
+                            <Text medium secondary onPress={() => this.toggleEdit('target')}>
+                                {editing === 'target' ? 'Save' : 'Edit'}
+                            </Text>
+                        </Block>
+                        <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                            <Block>
+                                <Text gray2 style={{marginBottom: 10}}>Budget</Text>
+                                {this.renderEdit('budget')}
+                            </Block>
+                            <Text medium secondary onPress={() => this.toggleEdit('budget')}>
+                                {editing === 'budget' ? 'Save' : 'Edit'}
+                            </Text>
+                        </Block>
+                        <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                            <Block>
+                                <Text gray2 style={{marginBottom: 10}}>Monthly Cap</Text>
+                                {this.renderEdit('monthlyCap')}
+                            </Block>
+                            <Text medium secondary onPress={() => this.toggleEdit('monthlyCap')}>
+                                {editing === 'monthlyCap' ? 'Save' : 'Edit'}
+                            </Text>
+                        </Block>
+                        <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                            <Block>
+                                <Text gray2 style={{marginBottom: 10}}>Target date</Text>
+                                {this.renderEdit('targetDate')}
+                            </Block>
+                            <Text medium secondary onPress={() => this.toggleEdit('targetDate')}>
+                                {editing === 'targetDate' ? 'Save' : 'Edit'}
+                            </Text>
+                        </Block>
+                    </Block>
+                    <Divider/>
+                    <Block style={styles.toggles}>
+                        <Block row center space="between" style={{marginBottom: theme.sizes.base * 2}}>
+                            <Text gray2>Withdraw money</Text>
+                            <Switch
+                                value={this.state.withdrawMoney}
+                                onValueChange={value => this.setState({withdrawMoney: value})}
+                            />
+                        </Block>
+
+                        <Block row center space="between" style={{marginBottom: theme.sizes.base * 2}}>
+                            <Text gray2>Change target date</Text>
+                            <Switch
+                                value={this.state.changeTargetDate}
+                                onValueChange={value => this.setState({changeTargetDate: value})}
+                            />
+                        </Block>
+
+                        <Block row center space="between" style={{marginBottom: theme.sizes.base * 2}}>
+                            <Text gray2>Change budget percentage</Text>
+                            <Switch
+                                value={this.state.changebudgetpercentage}
+                                onValueChange={value => this.setState({changebudgetpercentage: value})}
+                            />
+                        </Block>
+                    </Block>
+
+                </ScrollView>
             </Block>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  Location
-                </Text>
-                {this.renderEdit("location")}
-              </Block>
-              <Text
-                medium
-                secondary
-                onPress={() => this.toggleEdit("location")}
-              >
-                {editing === "location" ? "Save" : "Edit"}
-              </Text>
-            </Block>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  E-mail
-                </Text>
-                <Text bold>{profile.email}</Text>
-              </Block>
-            </Block>
-          </Block>
-
-          <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
-
-          <Block style={styles.sliders}>
-            <Block margin={[10, 0]}>
-              <Text gray2 style={{ marginBottom: 10 }}>
-                Budget
-              </Text>
-              <Text caption gray right>
-                $1,000
-              </Text>
-            </Block>
-            <Block margin={[10, 0]}>
-              <Text gray2 style={{ marginBottom: 10 }}>
-                Monthly Cap
-              </Text>
-              <Text caption gray right>
-                $5,000
-              </Text>
-            </Block>
-          </Block>
-
-          <Divider />
-
-          <Block style={styles.toggles}>
-            <Block
-              row
-              center
-              space="between"
-              style={{ marginBottom: theme.sizes.base * 2 }}
-            >
-              <Text gray2>Notifications</Text>
-              <Switch
-                value={this.state.notifications}
-                onValueChange={value => this.setState({ notifications: value })}
-              />
-            </Block>
-
-            <Block
-              row
-              center
-              space="between"
-              style={{ marginBottom: theme.sizes.base * 2 }}
-            >
-              <Text gray2>Newsletter</Text>
-              <Switch
-                value={this.state.newsletter}
-                onValueChange={value => this.setState({ newsletter: value })}
-              />
-            </Block>
-          </Block>
-        </ScrollView>
-      </Block>
-    );
-  }
+        )
+    }
 }
-
-Settings.defaultProps = {
-  profile: mocks.profile
-};
 
 export default Settings;
 
 const styles = StyleSheet.create({
-  header: {
-    paddingHorizontal: theme.sizes.base * 2
-  },
-  avatar: {
-    height: theme.sizes.base * 2.2,
-    width: theme.sizes.base * 2.2
-  },
-  inputs: {
-    marginTop: theme.sizes.base * 0.7,
-    paddingHorizontal: theme.sizes.base * 2
-  },
-  inputRow: {
-    alignItems: "flex-end"
-  },
-  sliders: {
-    marginTop: theme.sizes.base * 0.7,
-    paddingHorizontal: theme.sizes.base * 2
-  },
-  thumb: {
-    width: theme.sizes.base,
-    height: theme.sizes.base,
-    borderRadius: theme.sizes.base,
-    borderColor: "white",
-    borderWidth: 3,
-    backgroundColor: theme.colors.secondary
-  },
-  toggles: {
-    paddingHorizontal: theme.sizes.base * 2
-  }
-});
+    header: {
+        paddingHorizontal: theme.sizes.base * 2,
+    },
+    avatar: {
+        height: theme.sizes.base * 2.2,
+        width: theme.sizes.base * 2.2,
+    },
+    inputs: {
+        marginTop: theme.sizes.base * 0.7,
+        paddingHorizontal: theme.sizes.base * 2,
+    },
+    inputRow: {
+        alignItems: 'flex-end'
+    },
+    sliders: {
+        marginTop: theme.sizes.base * 0.7,
+        paddingHorizontal: theme.sizes.base * 2,
+    },
+    thumb: {
+        width: theme.sizes.base,
+        height: theme.sizes.base,
+        borderRadius: theme.sizes.base,
+        borderColor: 'white',
+        borderWidth: 3,
+        backgroundColor: theme.colors.secondary,
+    },
+    toggles: {
+        paddingHorizontal: theme.sizes.base * 2,
+    }
+})
--- frontend/screens/SignUp.js
+++ frontend/screens/SignUp.js
@@ -1,113 +1,135 @@
-import React, { Component } from 'react';
-import { Alert, ActivityIndicator, Keyboard, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {
+    Alert,
+    ActivityIndicator,
+    Keyboard,
+    KeyboardAvoidingView,
+    StyleSheet,
+    Linking,
+} from 'react-native';
 
-import { Button, Block, Input, Text } from '../components';
-import { theme } from '../constants';
+import {Button, Block, Input, Text} from '../components';
+import {theme} from '../constants';
 
 export default class SignUp extends Component {
-  state = {
-    email: null,
-    username: null,
-    password: null,
-    errors: [],
-    loading: false,
-  }
+    state = {
+        name: null,
+        dateofbirth: null,
+        errors: [],
+        loading: false,
+    };
 
-  handleSignUp() {
-    const { navigation } = this.props;
-    const { email, username, password } = this.state;
-    const errors = [];
+    handleSignUp() {
+        const {navigation} = this.props;
+        const {name, dateofbirth, password} = this.state;
+        const errors = [];
 
-    Keyboard.dismiss();
-    this.setState({ loading: true });
+        Keyboard.dismiss();
+        this.setState({loading: true});
 
-    // check with backend API or with some static data
-    if (!email) errors.push('email');
-    if (!username) errors.push('username');
-    if (!password) errors.push('password');
+        // check with backend API or with some static data
+        if (!name) errors.push('name');
+        if (!dateofbirth) errors.push('dateofbirth');
 
-    this.setState({ errors, loading: false });
+        this.setState({errors, loading: false});
 
-    if (!errors.length) {
-      Alert.alert(
-        'Success!',
-        'Your account has been created',
-        [
-          {
-            text: 'Continue', onPress: () => {
-              navigation.navigate('Browse')
-            }
-          }
-        ],
-        { cancelable: false }
-      )
+        if (!errors.length) {
+            Alert.alert(
+                'Success!',
+                'Your account has been created',
+                [
+                    {
+                        text: 'Continue',
+                        onPress: () => {
+                            navigation.navigate('Browse');
+                        },
+                    },
+                ],
+                {cancelable: false}
+            );
+        }
     }
-  }
 
-  render() {
-    const { navigation } = this.props;
-    const { loading, errors } = this.state;
-    const hasErrors = key => errors.includes(key) ? styles.hasErrors : null;
+    render() {
+        const {navigation} = this.props;
+        const {loading, errors} = this.state;
+        const hasErrors = key => (errors.includes(key) ? styles.hasErrors : null);
 
-    return (
-      <KeyboardAvoidingView style={styles.signup} behavior="padding">
-        <Block padding={[0, theme.sizes.base * 2]}>
-          <Text h1 bold>Sign Up</Text>
-          <Block middle>
-            <Input
-              email
-              label="Email"
-              error={hasErrors('email')}
-              style={[styles.input, hasErrors('email')]}
-              defaultValue={this.state.email}
-              onChangeText={text => this.setState({ email: text })}
-            />
-            <Input
-              label="Username"
-              error={hasErrors('username')}
-              style={[styles.input, hasErrors('username')]}
-              defaultValue={this.state.username}
-              onChangeText={text => this.setState({ username: text })}
-            />
-            <Input
-              secure
-              label="Password"
-              error={hasErrors('password')}
-              style={[styles.input, hasErrors('password')]}
-              defaultValue={this.state.password}
-              onChangeText={text => this.setState({ password: text })}
-            />
-            <Button gradient onPress={() => this.handleSignUp()}>
-              {loading ?
-                <ActivityIndicator size="small" color="white" /> :
-                <Text bold white center>Sign Up</Text>
-              }
-            </Button>
+        return (
+            <KeyboardAvoidingView style={styles.signup} behavior="padding" enabled={false}>
+                <Block padding={[0, theme.sizes.base * 2]}>
+                    <Text h1 bold style={styles.header}>
+                        Register
+                    </Text>
+                    <Input
+                        name
+                        label="Name"
+                        error={hasErrors('name')}
+                        style={[styles.input, hasErrors('name')]}
+                        defaultValue="Specify name"
+                        onChangeText={text => this.setState({name: text})}
+                    />
 
-            <Button onPress={() => navigation.navigate('Login')}>
-              <Text gray caption center style={{ textDecorationLine: 'underline' }}>
-                Back to Login
-              </Text>
-            </Button>
-          </Block>
-        </Block>
-      </KeyboardAvoidingView>
-    )
-  }
+                    <Input
+                        label="Date of Birth"
+                        error={hasErrors('dateofbirth')}
+                        style={[styles.input, hasErrors('dateofbirth')]}
+                        defaultValue="Specify date of birth"
+                        onChangeText={text => this.setState({dateofbirth: text})}
+                    />
+                    <Button gradient onPress={() => this.handleSignUp()}>
+                        {loading ? (
+                            <ActivityIndicator size="small" color="white"/>
+                        ) : (
+                            <Text bold white center>
+                                Register
+                            </Text>
+                        )}
+                    </Button>
+
+                    <Block center bottom>
+                        <Text caption center style={{color: "gray", margin: 0}}>
+                            Icons made by
+                        </Text>
+                        <Text center style={styles.links} onPress={() => {
+                            Linking.openURL('https://www.freepik.com/');
+                        }}>
+                            https://www.freepik.com/
+                        </Text>
+
+                        <Text center style={styles.links} onPress={() => {
+                            Linking.openURL('https://fontawesome.com/license');
+                        }}>
+                            https://fontawesome.com/license
+                        </Text>
+
+                    </Block>
+                </Block>
+            </KeyboardAvoidingView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  signup: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  input: {
-    borderRadius: 0,
-    borderWidth: 0,
-    borderBottomColor: theme.colors.gray2,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  hasErrors: {
-    borderBottomColor: theme.colors.accent,
-  }
-})
+    signup: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    input: {
+        borderRadius: 0,
+        borderWidth: 0,
+        borderBottomColor: theme.colors.gray2,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    hasErrors: {
+        borderBottomColor: theme.colors.accent,
+    },
+    links: {
+        color: 'green',
+        textDecorationLine: 'underline',
+    },
+    header: {
+        marginBottom:20,
+
+},
+});
